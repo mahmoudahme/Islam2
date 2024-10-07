4d7d69271dcc2b43fa5deceba800b818f4b135eb
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-10-07 15:57:52 +0300

    Update DbConnection.js

--- Config/DbConnection.js
+++ Config/DbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const DBConnection = ()=>{
-    mongoose.connect(process.env.DB_URL).then((connect)=>{
+    mongoose.connect("mongodb://127.0.0.1:27017/Islam").then((connect)=>{
         console.log(`DataBase Connected ${connect.connection.host}`)
     })
 }
